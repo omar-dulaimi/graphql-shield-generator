ca473b069222e0f54c48ef0f58a31100a54d8b72
omar-dulaimi <o.m.dulaimi@gmail.com>
2023-01-01 16:23:30 +0300

    remove file if exists

--- src/utils/writeFileSafely.ts
+++ src/utils/writeFileSafely.ts
@@ -1,8 +1,11 @@
 import fs from 'fs';
 import path from 'path';
 import { formatFile } from './formatFile';
+import removeDir from './removeDir';
 
 export const writeFileSafely = async (writeLocation: string, content: any) => {
+  await removeDir(writeLocation, true);
+
   fs.mkdirSync(path.dirname(writeLocation), {
     recursive: true,
   });
